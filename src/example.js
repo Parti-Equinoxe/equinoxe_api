edb31912711b7a178f825ca58ad578bf85f28843
Youri Duret <youri.duret@gmail.com>
2024-12-23 17:34:16 +0100

    ajout exemple (depuis ancien index)

---
+++ src/example.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const brevo = require("./brevo.js");
+const qomon = require("./qomon.js");
+
+
+(async () => {
+    console.log(await brevo.get("contacts/test_prenom_nom_tel_3@gmail.com", [{
+        label: "identifierType",
+        value: "email_id"
+    }]));
+    console.log(await qomon.post("search", {
+        "advanced_search": {
+            "per_page": 1,
+            query: {
+                "$all": [
+                    {
+                        "$at_least_one": [
+                            {
+                                "$condition": {
+                                    attr: "mail",
+                                    ope: "eql",
+                                    value: "test_prenom_nom_tel_3@gmail.com"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    }))
+})()
+
