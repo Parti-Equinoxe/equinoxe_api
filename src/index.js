d4b6e6a9fb1c76ca4abe0954aedc0733d7ad63de
Youri Duret <youri.duret@gmail.com>
2024-12-23 09:55:14 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,36 +1,56 @@
 require("dotenv").config();
 const axios = require("axios");
-
-async function brevo(path) {
-    const res = await axios.get(`https://api.brevo.com/v3/${path}`, {
-        headers: {
-            "accept": "application/json",
-            "api-key": process.env.TOKEN_BREVO,
-            "content-type": "application/json"
-        }
-    }).catch(e => {
-        console.log(e)
-    });
-    console.log(res.data);
-    return res.data
-}
+const brevo = require("./brevo.js");
 
 async function qomon(path) {
-    const res = await axios.get(`https://incoming.qomon.app/${path}`, {
-        headers: {
-            "accept": "application/json",
-            "Authorization": `Bearer ${process.env.TOKEN_QOMON}`,
-            "content-type": "application/json",
-            "user-agent": "equinoxe/api"
+    const res = await axios.post(`https://incoming.qomon.app/${path}`, {
+            data: {
+                "advanced_search": {
+                    "per_page": 1,
+                    query: {
+                        "$all": [
+                            {
+                                "$at_least_one": [
+                                    {
+                                        "$condition": {
+                                            attr: "mail",
+                                            ope: "eql",
+                                            value: "test_prenom_nom_tel_3@gmail.com"
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                }
+            }
         },
-    }).catch(e => {
+        {
+            headers: {
+                "accept": "application/json",
+                "Authorization": `Bearer ${process.env.TOKEN_QOMON}`,
+                "content-type": "application/json",
+                "user-agent": "equinoxe/api"
+            },
+        }).catch(e => {
         console.log(e)
         console.log(`https://incoming.qomon.app/${path}`)
         return {data: e.toString()}
     });
-    console.log(res.data.data.contact);
+    console.log(res.data.data.contacts[0]);
     return res.data
 }
 
-qomon("contacts/127503978")
+(async () => {
+    //brevo("contacts?filter=equals(EMAIL,\"test_prenom_nom_tel_3@gmail.com\")")
+    console.log(await brevo.get("contacts/test_prenom_nom_tel_3@gmail.com", [{
+        label: "identifierType",
+        value: "email_id"
+    }]));
+    console.log(await brevo.get("contacts/test_prenom_nom_tel_2@gmail.com", [{
+        label: "identifierType",
+        value: "email_id"
+    }]));
+    //await qomon("search")//contacts/127503978
+})()
 
