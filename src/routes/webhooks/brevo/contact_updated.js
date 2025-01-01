3298e1664685ea94b7da60481373b8eb8e7d081c
Youri Duret <youri.duret@gmail.com>
2025-01-01 18:26:17 +0100

    maj

--- src/routes/webhooks/brevo/contact_updated.js
+++ src/routes/webhooks/brevo/contact_updated.js
@@ -1,6 +1,17 @@
 module.exports = {
-    exec(req) {
-        //penser à ajouter verif ip
-        return {message: "Bienvenu sur l'API Équinoxe !"};
+    method: "POST",
+    /**
+     * @param {Request} req
+     * @param {Response} res
+     * @return {{message: string}}
+     */
+    async exec(req, res) {
+        const data = req.body;
+        console.log(data);
+        if (!data || data.event !== "contact_updated") return {error: "Bad event."};
+        if (data.content.hasOwnProperty("link_list_ids")) {
+
+        }
+        return {message: "Nice"};
     }
 }
