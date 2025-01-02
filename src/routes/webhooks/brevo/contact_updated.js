34dc3075ab59c4a9197be29143d83ede116c65a4
Youri Duret <youri.duret@gmail.com>
2025-01-02 20:12:55 +0100

    site dev, mapping champs brevo/qomon

--- src/routes/webhooks/brevo/contact_updated.js
+++ src/routes/webhooks/brevo/contact_updated.js
@@ -1,4 +1,4 @@
-const status = require("../../../api/status.json");
+const mapping = require("../../../api/mapping.json");
 const qomon = require("../../../api/qomon");
 module.exports = {
     method: "POST",
@@ -12,44 +12,39 @@ module.exports = {
         console.dir(data, {depth: null});
         if (!data || data.event !== "contact_updated" || !data.content) return {error: "Bad event."};
         let done = false;
-        if (data.content.hasOwnProperty("link_list_ids") || data.content.hasOwnProperty("unlink_list_ids")) {
-            const type = {
-                ajout: (data.content["link_list_ids"] ?? []).filter(id => status.filters.brevo.includes(id)).map((id) => status.status.find((s) => s.id_brevo === id)),
-                suppression: (data.content["unlink_list_ids"] ?? []).filter(id => status.filters.brevo.includes(id)).map((id) => status.status.find((s) => s.id_brevo === id))
-            };//filtrage des id des list pour avoir seulement les lists que l'on veut mettre a jour
-            console.log(type);
-            for (const email of data.content.emails) {
-                const userID = await qomon.getID(email);
-                if (userID === "0") continue; //créer le contact
-                console.log(userID);
-                const r = await qomon.patch(`contacts/${userID}`,{
-                    //firstname: "Test_liste",
-                    status: type.ajout.map((s) => {
-                        return {
-                            label: "Niveau de soutien",
-                            value: s.id_qomon
-                        }
-                    })
-                });
-                console.log(r)
-                for (const list of type.ajout) {
-                    //code pour ajouter le status sur qomon
-                    console.log(`Ajout de ${email} sur la list ${list.name}`);
-                    done = true;
+        const content = [data.content].flat();
+        console.log(content);
+        for (const atr of content) {
+            /*const atrAsTab = Object.keys(atr).map((f)=>{
+                return {
+                    name: f,
+                    value: atr[f]
                 }
-                for (const list of type.suppression) {
-                    //code pour supprimer le status sur qomon
-                    console.log(`Suppression de ${email} sur la list ${list.name}`);
-                    done = true;
-                }
+            });
+            console.log(atrAsTab);*/
+            const fiels = mapping.filter((key) => callFromString(atr, key.brevo));
+            let newData = {};
+            for (const field of fiels) {
+                newData[field.qomon] = callFromString(atr, field.brevo);
             }
+            console.log(newData);
+            const r = await qomon.updateContact(atr.email, newData);
+            if (r.error) {
+                console.log(r);
+            }
+            done=true;
         }
-        //console.log(data.content.length);
-        //console.log(data.content[0]);
-        /*if (typeof data.content === "object") {
-            const userID = getID()
-        }*/
         if (!done) return {message: "No modification required."};
-        return {message: "Nice"};
+        return {message: "Contact updated"};
     }
+}
+
+function callFromString(object, path) {
+    const keys = path.split(".");
+    let val = object;
+    for (const key of keys) {
+        if (!val[key]) return undefined;
+        val = val[key];
+    }
+    return val;
 }
