e81feebfe75864a68ddc615e66007650785e6f10
Youri Duret <youri.duret@gmail.com>
2025-01-03 11:01:08 +0100

    reactivatuion verif token mais n'annule pas la requet

--- src/index.js
+++ src/index.js
@@ -42,14 +42,14 @@ async function init() {
         app[route.method.toLowerCase()](route.route, async (req, res) => {
             const date = Date.now();
             let result = {};
-            if (false && !verifyToken(req.headers)) {
+            if (!verifyToken(req.headers)) {
                 console.log(redBright(`>> Unauthorized access to ${route.route} from ${req.headers.host} (${req.ip})`));
-                return res.status(401).send({
+                /*return res.status(401).send({
                     state: "Unauthorized",
                     status: 401,
                     message: "You need to add authorization: Bearer <token> in the header.",
                     temps: Date.now() - date,
-                });
+                });*/
             }
             try {
                 result = await route.exec(req, res);
