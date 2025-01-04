effad5e1721a66211f98e942332b1f212a20726c
Youri Duret <youri.duret@gmail.com>
2025-01-04 18:09:29 +0100

    activé créa compte
    mais désactiver dans sa fct

--- src/routes/webhooks/brevo/list_addition.js
+++ src/routes/webhooks/brevo/list_addition.js
@@ -48,8 +48,8 @@ module.exports = {
             });
             if (r.error === "Contact not found") {
                 console.log(email);
-                //const rc = await qomon.createContact(email);
-                //if (rc.error) console.log(redBright(`Error creating contact ${email}: ${JSON.stringify(rc)}`));
+                const rc = await qomon.createContact(email);
+                if (rc.error) console.log(redBright(`Error creating contact ${email}: ${JSON.stringify(rc)}`));
             } else if (r.error) console.log(redBright(`Error updating contact ${email}: ${JSON.stringify(r)}`));
             count++;
         }
