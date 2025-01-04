035e62549cb6a1585b0ca3d21b4a86973ce97084
Youri Duret <youri.duret@gmail.com>
2025-01-04 19:39:25 +0100

    ajout login nessecaire

--- src/routes/discord/login.js
+++ src/routes/discord/login.js
@@ -1,5 +1,6 @@
 module.exports = {
     method: "GET",
+    token: true,
     exec: (req, res) => {
         const authUrl = `https://discord.com/api/oauth2/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.REDIRECT_URI)}&response_type=code&scope=identify`;
         res.redirect(authUrl);
