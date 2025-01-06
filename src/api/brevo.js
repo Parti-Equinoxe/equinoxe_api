d94c24813803c0708e3e1321a63082be3226cec9
Youri Duret <youri.duret@gmail.com>
2025-01-06 17:16:47 +0100

    amelioration getStatus + ajout getContactFromDiscord + doc

--- src/api/brevo.js
+++ src/api/brevo.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const link = "https://api.brevo.com/v3/";
+const status = require("./status.json");
 
 /**
  * Configuration de l'api brevo
@@ -18,7 +19,7 @@ function formatError(e) {
 }
 
 /**
- * Permet de faire une requete sur l'api brevo
+ * Permet de faire une requete (GET) sur l'api brevo
  * @param {string} path - l'action à effectuer (voir doc brevo)
  * @param {Array<{label: string, value: string}>} parameters - les parametres eventuels
  * @return {Promise<Object>}
@@ -28,6 +29,12 @@ module.exports.get = async (path, parameters = []) => {
         this.config
     ).catch(formatError)).data;
 }
+/**
+ * Permet de faire une requete (PUT) sur l'api brevo
+ * @param {string} path - l'action à effectuer (voir doc brevo)
+ * @param {Array<{label: string, value: string}>} parameters - les parametres eventuels
+ * @return {Promise<Object>}
+ */
 module.exports.put = async (path, data, parameters = []) => {
     return (await axios.put(`${link}${path}${parameters.length > 0 ? "?" + parameters.map(p => `${p.label}=${p.value}`) : ""}`,
         data,
@@ -46,8 +53,22 @@ module.exports.getContact = async (email) => {
 
 /**
  * Permet de recupérer les informations d'un contact
+ * @param {string} userID - l'id discord
+ * @return {Promise<Object>}
+ */
+module.exports.getContactFromDiscord = async (userID) => {
+    const resp = (await this.get("contacts/", [{
+        label: "filter",
+        value: `equals(DISCORD_ID,"${userID}")`
+    }])).data;
+    console.dir(resp, {death: null});
+    return resp.find((c) => c.attributes.DISCORD_ID === userID);
+}
+
+/**
+ * Permet de mettre a jour les informations d'un contact
  * @param {string} email
- * @param {string} data
+ * @param {string} data - les donnes a mettre a jours
  * @return {Promise<Object>}
  */
 module.exports.updateContact = async (email, data) => {
@@ -56,12 +77,24 @@ module.exports.updateContact = async (email, data) => {
 
 /**
  * Permet de recupérer le status d'un contact
- * @param {{email: string}|{userID:String}} identifier
- * @return {Promise<Object>}
+ * @param {{email: string}|{userID: string}|{id: string}} identifier
+ * @return {Promise<{adh: boolean, symp: boolean}>}
  */
 module.exports.getStatus = async (identifier) => {
-    if (identifier.hasOwnProperty("userID")) {
-
+    let resp;
+    if (identifier.hasOwnProperty("email")) {
+        resp = await this.getContact(identifier.email);
     }
-    return;
+    if (identifier.hasOwnProperty("userID")) {
+        resp = await this.getContactFromDiscord(identifier.userID);
+    }
+    if (identifier.hasOwnProperty("ID")) {
+        resp = (await this.get("contacts/" + identifier.ID, [{label: "identifierType", value: "contact_id"}]));
+    }
+    if (!resp) resp = {listIds: []};
+    const lists = resp.listIds.filter(id => status.filters.brevo.includes(id)).map((id) => status.status.find((s) => s.id_brevo === id));
+    return {
+        adh: !!lists.find(l => l.name === "Adhérents"),
+        symp: !!lists.find(l => l.name === "Sympathisants")
+    };
 }
