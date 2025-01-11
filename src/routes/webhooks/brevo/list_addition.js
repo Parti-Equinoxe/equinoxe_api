7487ad6b0f09564c676830e13a97f2a5fe10975d
Youri Duret <youri.duret@gmail.com>
2025-01-11 15:36:54 +0100

    ajout maj metaData

--- src/routes/webhooks/brevo/list_addition.js
+++ src/routes/webhooks/brevo/list_addition.js
@@ -1,6 +1,5 @@
-const status = require("../../../api/lists_brevo.json");
-const qomon = require("../../../api/qomon");
-const {redBright} = require("cli-color");
+const {transformList, getContact} = require("../../../api/brevo");
+const {getToken, pushMetaData, saveToken} = require("../../../api/discord");
 //const form_NS_id = 82467;
 module.exports = {
     token: true,
@@ -15,8 +14,21 @@ module.exports = {
         console.dir(data, {depth: null});
         if (!data || data.event !== "list_addition" || !data.list_id || (!data.emails && !data.email)) return {error: "Bad event."};
         let count = 0;
-        const list_ids = data.list_id.filter(id => status.filters.brevo.includes(id)).map((id) => status.status.find((s) => s.id_brevo === id))
+        //const list_ids = data.list_id.filter(id => status.filters.brevo.includes(id)).map((id) => status.status.find((s) => s.id_brevo === id));
         for (const email of (data.emails ?? [data.email])) {
+            //DISCORD
+            let token = getToken({email: email});
+            console.log(token);
+            if (!token) continue;
+            const userData = await getContact(email);
+            console.log(userData);
+            const list = transformList(userData.listIds);
+            const res = await pushMetaData(userData.attributes.DISCORD_ID, token,{
+                adh: +(list.adherents ?? 0),
+                symp: +(list.sympathisants ?? 0)
+            });
+            await saveToken(userData.attributes.DISCORD_ID, email, res.token);
+            //FIN DISCORD
             //code pour verif les niveaux, mit en pause pour le moment
             /*const userNS = (await qomon.getContact(email)).formdatas.find(f => f.form_id === form_NS_id) ?? {data: "None"};
             const NS = status.status.find(s => s.id_qomon.toLowerCase() === userNS.data) ?? {
@@ -39,7 +51,7 @@ module.exports = {
                     if (rc.error) console.log(redBright(`Error creating contact ${email}: ${JSON.stringify(rc)}`));
                 } else if (r.error) console.log(redBright(`Error updating contact ${email}: ${JSON.stringify(r)}`));
             }*/
-            const r = await qomon.updateContact(email,{
+            /*const r = await qomon.updateContact(email,{
                 status: list_ids.map((list) => {
                     return {
                         label: "Niveau de soutien",
@@ -51,7 +63,7 @@ module.exports = {
                 console.log(email);
                 const rc = await qomon.createContact(email);
                 if (rc.error) console.log(redBright(`Error creating contact ${email}: ${JSON.stringify(rc)}`));
-            } else if (r.error) console.log(redBright(`Error updating contact ${email}: ${JSON.stringify(r)}`));
+            } else if (r.error) console.log(redBright(`Error updating contact ${email}: ${JSON.stringify(r)}`));*/
             count++;
         }
         if (count === 0) return {message: "No modification required."};
