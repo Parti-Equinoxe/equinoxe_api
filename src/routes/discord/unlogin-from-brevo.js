198d1d3d1b793ab777f6a75374b601fee02f2658
Youri Duret <youri.duret@gmail.com>
2025-01-11 15:36:42 +0100

    dev + redirect vers unlogin-from-discord

--- src/routes/discord/unlogin-from-brevo.js
+++ src/routes/discord/unlogin-from-brevo.js
@@ -1,5 +1,5 @@
 const {removeMetaData, getToken} = require("../../api/discord");
-const {getContactFromId} = require("../../api/brevo");
+const {getContactFromId, updateContact, isLinked} = require("../../api/brevo");
 module.exports = {
     method: "GET",
     token: true,
@@ -7,10 +7,24 @@ module.exports = {
         if (!req.query.identifier) return {error: "No identifier provided."}; //TODO: page d'erreur
         const userData = await getContactFromId(req.query.identifier);
         if (userData.error) return userData //TODO: renvoyer vers une page d'erreur
-        const token = getToken({email:userData.email});
-        if (!token) return {error: "No Discord account linked"};
+        if (!(await isLinked(userData.email, userData.attributes.DISCORD_ID))) return {message: "No Discord account linked"};
+        return res.redirect("/discord/unlogin-from-discord?identifier=" + req.query.identifier);
+        /*const token = await getToken({email: userData.email});
+        if (!token) return {message: "No Discord account linked"};
         const result = await removeMetaData(userData.attributes.DISCORD_ID, token);
-        if (result.error) return {error: "Error when removing metaData", full: result.error}; //TODO: renvoyer vers une page d'erreur
-        return {message: "Déconnexion réussie"} //TODO: renvoyer vers une page indiquant que la deconnection est reussite
+        //console.log(result.data);
+        if (result.data.error) return { // ne fct pas ...
+            error: "Error when removing metaData",
+            full: result.error,
+            message: result.data.error
+        }; //TODO: renvoyer vers une page d'erreur
+        const resBrevo = await updateContact(userData.email, {
+            attributes: {
+                DISCORD_ID: "",
+                DISCORD_REFRESH_TOKEN: ""
+            }
+        });
+        if (resBrevo.error) return {error: resBrevo.error, full: resBrevo} //TODO: renvoyer vers une page d'erreur
+        return {message: "Déconnexion réussie"} //TODO: renvoyer vers une page indiquant que la deconnection est reussite*/
     }
 }
