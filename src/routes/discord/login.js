3ba850f1a1881651818f620bdc476148ef33cedf
Youri Duret <youri.duret@gmail.com>
2025-01-13 10:10:56 +0100

    verif si pas déjà co

--- src/routes/discord/login.js
+++ src/routes/discord/login.js
@@ -1,16 +1,19 @@
+const {getContactFromId} = require("../../api/brevo");
 const redirectURL = "https://youri.cleboost.com/discord/callback-login";
 //TODO: code pour exporter les adh actuels
 module.exports = {
     method: "GET",
     token: true,
-    exec: (req, res) => {
+    exec: async (req, res) => {
         if (!req.query.identifier) return {error: "No identifier provided."}; //TODO: page d'erreur
+        const identifier = req.query.identifier;
+        const userData = await getContactFromId(identifier);
+        if (userData.attributes.DISCORD_ID) return {message: "This contact is already linked to a Discord account"}
         const state = crypto.randomUUID(); // permet d'augementer la sécurité
         const authUrl = `https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=${encodeURIComponent(redirectURL)}&response_type=code&scope=identify+role_connections.write&state=${state}`;
         res.cookie("state", state, {maxAge: 1000 * 60 * 5, signed: true});
         //id brevo
-        console.log(req.query.identifier);
-        res.cookie("identifier", req.query.identifier, {maxAge: 1000 * 60 * 5, signed: true}); //TODO: est t il possible faire de plus securise ?
+        res.cookie("identifier", identifier, {maxAge: 1000 * 60 * 5, signed: true}); //TODO: est t il possible faire de plus securise ?
         res.redirect(authUrl);
         return {message: "Redirection vers Discord"};
     }
