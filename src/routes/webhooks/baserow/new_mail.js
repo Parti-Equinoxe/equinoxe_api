7beb8dfbb0ef1f38c8ef7de4491f0941b1a30e54
youritch <youri.duret@gmail.com>
2025-04-18 14:18:48 +0200

    fix(webhook mail): clean up + configurabilité

--- src/routes/webhooks/baserow/new_mail.js
+++ src/routes/webhooks/baserow/new_mail.js
@@ -1,4 +1,11 @@
 const axios = require("axios");
+const champs ={
+    description: "Corps du texte",
+    title: "Objet",
+    footer: "Mail envoi",
+    date: "Date d'envoi"
+};
+
 module.exports = {
     method: "POST",
     /**
@@ -13,15 +20,15 @@ module.exports = {
         if (data.event_type !== "rows.created") return {error: "Bad event type."};
         let count = 0;
         for (const mail of data.items) {
-            if (!mail["Corps du texte"]) continue;
+            if (!mail[champs.description]) continue;
             const date= new Date(Date.now())
-            const dateEnvoi = new Date(mail["Date d'envoi"]);
+            const dateEnvoi = new Date(mail[champs.date]);
             const embed = {
-                title: `${mail["Objet"] ?? "Nouveau Mail :"} - ${dateEnvoi.getDate()}/${dateEnvoi.getMonth()+1}/${dateEnvoi.getFullYear()}`.slice(0,99),
-                description: mail["Corps du texte"].slice(0, 4000),
+                title: `${mail[champs.title] ?? "Nouveau Mail :"} - ${dateEnvoi.getDate()}/${dateEnvoi.getMonth()+1}/${dateEnvoi.getFullYear()}`.slice(0,99),
+                description: mail[champs.description].slice(0, 4000),
                 timestamp: date.toISOString(),
                 color: parseInt("19171C", 16),
-                footer:{text:( mail["Mail envoi"] ?? "").toString() ?? null}
+                footer:{text:( mail[champs.footer] ?? "").toString() ?? null}
             };
             await axios.post(process.env.DISCORD_WEBHOOK_RELECTURE, {
                 embeds: [embed]
