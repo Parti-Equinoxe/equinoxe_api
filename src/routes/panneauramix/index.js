1877e13bb963768b59119cc3bd87a468f4cab41a
youritch <youri.duret@gmail.com>
2025-06-04 17:19:07 +0200

    feat(panneauramix): index route

--- src/routes/panneauramix/index.js
+++ src/routes/panneauramix/index.js
@@ -1,6 +1,7 @@
+const msg = require("../../panneauramix/config.json").message;
 module.exports = {
     method: "GET",
     exec(req, res) {
-        return {message: "Panneauramix !!"}
+        return {message: msg};
     }
 }
